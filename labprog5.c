4e0533fdc3577e2643e8b748f5412c9ec5feffb3
sripriya2002 <68781421+sripriya2002@users.noreply.github.com>
2022-07-19 15:52:14 +0530

    Update labprog5.c

--- labprog5.c
+++ labprog5.c
@@ -1,84 +1,71 @@
 #include<stdio.h>
 #include<GL/glut.h>
-float v[4][3]={{0.0,0.0,1.0},{0.0,1.0,0.0},{-1.0,-0.5,0.0},{1.0,-0.5,0.0}};
-float colors[4][3]={{1.0,0.0,0.0},{0.0,1.0,0.0},{0.0,0.0,1.0},{0.0,0.0,0.0}};
+typedef float point[3];
 int n;
-void triangle(float *va,float *vb,float *vc)
-{
+point v[4][3]={{0.0,0.0,1.0},{0.0,1.0,0.0},{-1.0,-0.5,0.0},{1.0,-0.5,0.0}};
+void triangle(point a,point b,point c){
 	glBegin(GL_TRIANGLES);
-		glVertex3fv(va);
-		glVertex3fv(vb);
-		glVertex3fv(vc);
+		glVertex3fv(a);
+		glVertex3fv(b);
+		glVertex3fv(c);
+	glEnd();
 }
-void tetra(float *a,float *b,float *c,float *d)
-{
-	glColor3fv(colors[0]);
-	triangle(a,b,c);
-	glColor3fv(colors[1]);
-	triangle(a,c,d);
-	glColor3fv(colors[2]);
-	triangle(a,d,b);
-	glColor3fv(colors[3]);
-	triangle(b,d,c);
-}
-void divide_tetra(float *a,float *b,float *c,float *d,int m)
-{
-	float mid[6][3];
-	int j;
-	if(m>0)
-	{
-		for(j=0;j<3;j++)
-			mid[0][j]=(a[j]+b[j])/2;
-		for(j=0;j<3;j++)
-			mid[1][j]=(a[j]+c[j])/2;
-		for(j=0;j<3;j++)
-			mid[2][j]=(a[j]+d[j])/2;
-		for(j=0;j<3;j++)
-			mid[3][j]=(b[j]+c[j])/2;
-		for(j=0;j<3;j++)
-			mid[4][j]=(c[j]+d[j])/2;
-		for(j=0;j<3;j++)
-			mid[5][j]=(b[j]+d[j])/2;
-		divide_tetra(a,mid[0],mid[1],mid[2],m-1);
-		divide_tetra(mid[0],b,mid[3],mid[5],m-1);
-		divide_tetra(mid[1],mid[3],c,mid[4],m-1);
-		divide_tetra(mid[2],mid[5],mid[4],d,m-1);
+void divide_tetra(point a,point b,point c,int m){
+	point v1,v2,v3;
+	int i;
+	if(m>0){
+		for(i=0;i<3;i++){
+			v1[i]=(a[i]+b[i])/2;
+			v2[i]=(a[i]+c[i])/2;
+			v3[i]=(b[i]+c[i])/2;
+		}
+		divide_tetra(a,v1,v2,m-1);
+		divide_tetra(c,v2,v3,m-1);
+		divide_tetra(b,v3,v1,m-1);
 	}
-	else{
-		tetra(a,b,c,d);
-	}
+	else
+		triangle(a,b,c);
 }
-void display(void)
-{
+void tetrahedron(int m){
+	glColor3f(1.0,0.0,0.0);
+	divide_tetra(v[0],v[1],v[2],m);
+	glColor3f(0.0,1.0,0.0);
+	divide_tetra(v[3],v[2],v[1],m);
+	glColor3f(0.0,0.0,1.0);
+	divide_tetra(v[0],v[3],v[1],m);
+	glColor3f(0.0,0.0,0.0);
+	divide_tetra(v[0],v[2],v[3],m);
+}
+void display(){
 	glClearColor(1.0,1.0,1.0,1.0);
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-	divide_tetra(v[0],v[1],v[2],v[3],n);
-	glEnd();
+	tetrahedron(n);
 	glFlush();
 }
-void myReshape(int w,int h)
-{
+void myReshape(int w,int h){
 	glViewport(0,0,w,h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	if(w<=h)
+	if(w<=h){
 		glOrtho(-2.0,2.0,-2.0*(float)h/(float)w,2.0*(float)h/(float)w,-10.0,10.0);
-	else
+	}
+	else{
 		glOrtho(-2.0*(float)w/(float)h,2.0*(float)w/(float)h,-2.0,2.0,-10.0,10.0);
+	}
 	glMatrixMode(GL_MODELVIEW);
 	glutPostRedisplay();
 }
-int main(int argc,char *argv[])
-{
-	printf("enter the no of divisions");
+void main(int argc,char *argv[]){
+	printf("enter the no of divivsions\n");
 	scanf("%d",&n);
 	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
-	glutInitWindowSize(500,500);
-	glutCreateWindow("3d gasket(4MT19CS161)");
+	glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE|GLUT_DEPTH);
+	glutInitWindowPosition(0,0);
+	glutInitWindowSize(700,700);
+	glutCreateWindow("tetrahedron");
 	glutReshapeFunc(myReshape);
 	glutDisplayFunc(display);
 	glEnable(GL_DEPTH_TEST);
 	glutMainLoop();
-	return 0;
 }
+	
